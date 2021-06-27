49b20716566e9e971d15c431b6e0b26f02e04a31
manvi2405 <86182564+manvi2405@users.noreply.github.com>
2021-06-27 12:06:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const button = document.getElementById("button");
+const red = document.getElementById("red");
+const redimage = document.getElementById("red-image");
+const blueimage = document.getElementById("blue-image");
+const yellowimage = document.getElementById("yellow-image");
+button.onclick = () => {
+    if(red.checked){
+        redimage.classList.remove("hidden");
+        redimage.classList.add("eject");
+
+    }
+    if(blue.checked){
+        blueimage.classList.remove("hidden");
+        blueimage.classList.add("eject");
+
+    }
+    if(yellow.checked){
+        yellowimage.classList.remove("hidden");
+        yellowimage.classList.add("eject");
+
+    }
+};
